23044a65a100f0cffc026371cb73b405eb7630d0
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-05 20:08:22 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export default [
+    {
+        title: "Mt. Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Machu picchu",
+        location: "Peru",
+        googleMapsUrl: "https://goo.gl/maps/11Z8CxwmUJV2MfueA",
+        goalDate: "24 Jan, 2021",
+        description: "Iconic hilltop ruins of a large 15th-century Inca city featuring numerous structures & terraces.",
+        imageUrl: "https://source.unsplash.com/4hMET7vYTAQ"
+    },
+    {
+        title: "Bagan",
+        location: "Myanmar",
+        googleMapsUrl: "https://goo.gl/maps/hPMUsQCKJ2robBof9",
+        goalDate: "12 Jan, 2021",
+        description: "Bagan is an ancient city and a UNESCO World Heritage Site in the Mandalay Region of Myanmar. From the 9th to 13th centuries, the city was the capital of the Bagan Kingdom,",
+        imageUrl: "https://source.unsplash.com/3DuDyX5Fpuk"
+    },
+    {
+        title: "Patagonia",
+        location: "Argentina",
+        googleMapsUrl: "https://goo.gl/maps/uis3anBjsD6LsGSKA",
+        goalDate: "12 Jan, 2021",
+        description: "Patagonia is a region encompassing the vast southernmost tip of South America, shared by Argentina and Chile, with the Andes Mountains as its dividing line.",
+        imageUrl: "https://source.unsplash.com/WKlkL2EiBE4"
+    },
+    {
+        title: "Kathmandu",
+        location: "Nepal",
+        googleMapsUrl: "https://goo.gl/maps/4B2L4662cfHEakSK8",
+        goalDate: "12 Jan, 2021",
+        description: "Kathmandu, Nepal's capital, is set in a valley surrounded by the Himalayan mountains.",
+        imageUrl: "https://source.unsplash.com//59Al83Zjtf8"
+    },
+]
