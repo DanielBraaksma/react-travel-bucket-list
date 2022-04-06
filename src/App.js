2a74d874f26a0452cc262b623fc06927df0882e9
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-06 13:37:01 -0400

    added props to Card component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <section>
+      <section className="main-container">
         {cards}
       </section>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,18 +1,9 @@
 export default [
     {
-        title: "Mt. Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    },
-    {
         title: "Machu picchu",
         location: "Peru",
         googleMapsUrl: "https://goo.gl/maps/11Z8CxwmUJV2MfueA",
-        goalDate: "24 Jan, 2021",
+        goalDate: "Goal date: Fall 2022",
         description: "Iconic hilltop ruins of a large 15th-century Inca city featuring numerous structures & terraces.",
         imageUrl: "https://source.unsplash.com/4hMET7vYTAQ"
     },
@@ -20,7 +11,7 @@ export default [
         title: "Bagan",
         location: "Myanmar",
         googleMapsUrl: "https://goo.gl/maps/hPMUsQCKJ2robBof9",
-        goalDate: "12 Jan, 2021",
+        goalDate: "Goal date: Summer 2023",
         description: "Bagan is an ancient city and a UNESCO World Heritage Site in the Mandalay Region of Myanmar. From the 9th to 13th centuries, the city was the capital of the Bagan Kingdom,",
         imageUrl: "https://source.unsplash.com/3DuDyX5Fpuk"
     },
@@ -28,7 +19,7 @@ export default [
         title: "Patagonia",
         location: "Argentina",
         googleMapsUrl: "https://goo.gl/maps/uis3anBjsD6LsGSKA",
-        goalDate: "12 Jan, 2021",
+        goalDate: "Goal date: Summer 2025",
         description: "Patagonia is a region encompassing the vast southernmost tip of South America, shared by Argentina and Chile, with the Andes Mountains as its dividing line.",
         imageUrl: "https://source.unsplash.com/WKlkL2EiBE4"
     },
@@ -36,8 +27,16 @@ export default [
         title: "Kathmandu",
         location: "Nepal",
         googleMapsUrl: "https://goo.gl/maps/4B2L4662cfHEakSK8",
-        goalDate: "12 Jan, 2021",
+        goalDate: "Goal date: Winter 2026",
         description: "Kathmandu, Nepal's capital, is set in a valley surrounded by the Himalayan mountains.",
         imageUrl: "https://source.unsplash.com//59Al83Zjtf8"
     },
+    {
+        title: "Mt. Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        goalDate: "Goal date: Fall 2028",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    }
 ]
