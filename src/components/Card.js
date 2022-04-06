4e15a5814831af0272e9c0b22f15507e38be781d
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-06 14:47:29 -0400

    added responsive css and removed last hr

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,21 @@
 import React from "react"
 
-export default function Card (props) {
+export default function Card(props) {
     console.log(props)
     return (
-         <div className="card">
-            <img className="card-img" src={props.item.imageUrl} />
-            <div className="card-text">
-                <img className="location-pin" src="./images/location.png" />
-                <h3>{props.item.location}</h3>
-                <a href={props.item.googleMapsUrl}>View on Google Maps</a>
-                <h2>{props.item.title}</h2>
-                <p className="goal-date">{props.item.goalDate}</p>
-                <p className="description">{props.item.description}</p>
+        <div>
+            <div className="card">
+                <img className="card-img" src={props.item.imageUrl} />
+                <div className="card-text">
+                    <img className="location-pin" src="./images/location.png" />
+                    <h3>{props.item.location}</h3>
+                    <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+                    <h2>{props.item.title}</h2>
+                    <p className="goal-date">{props.item.goalDate}</p>
+                    <p className="description">{props.item.description}</p>
+                </div>
             </div>
-         </div>
+            <hr></hr>
+        </div>
     )
 }
