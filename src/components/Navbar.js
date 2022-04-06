16a7719414e7085d2aa44deb4d11c36f75b5e2d8
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-05 21:50:47 -0400

    finished header

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar () {
+    return (
+        <header>
+            <img className="logo" src="./images/world.png" alt="icon of the world" />
+            <h1>My Bucket List</h1>
+        </header>
+    )
+}
